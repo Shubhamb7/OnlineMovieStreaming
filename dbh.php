888a9220129864a364d6226380cb9fef254a77a9
Shubham Belgaonkar <shubhamb756@gmail.com>
2020-10-20 20:47:27 +0530

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = mysqli_connect("us-cdbr-east-02.cleardb.com/heroku_da19d50f9fb9eea?reconnect=true","b226643cd887f1","7fe23f7f","users");
+  $conn = mysqli_connect("us-cdbr-east-02.cleardb.com","b226643cd887f1","7fe23f7f","heroku_da19d50f9fb9eea");
   if(! $conn ) {
       die('Could not connect: ' . mysqli_error());
    }
